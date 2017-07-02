0debfbe0ba4516e3b0543f6f3787c0d579a47530
Meelik Kiik <kiik.meelik@gmail.com>
2017-07-03 01:50:28 +0300

    Implement localized pose methods.

--- system/inc/tuum_localizer.hpp
+++ system/inc/tuum_localizer.hpp
@@ -2,6 +2,8 @@
 #ifndef TUUM_LOCALIZER_H
 #define TUUM_LOCALIZER_H
 
+#include "Vec.hpp"
+
 #include "tuum_system.hpp"
 
 #include "loc_landmark_stream.hpp"
@@ -9,6 +11,14 @@
 using namespace loc;
 
 namespace tuum {
+
+  class Navigator;
+
+  struct localized_pose_t {
+    Vec2i coord;
+    float orient;
+    uint32_t map_id;
+  };
 
   class Localizer : public Subsystem
   {
@@ -24,13 +34,22 @@ namespace tuum {
 
     Subsystem::Type getType() { return Localizer::GetType(); }
 
+    int updateGlobalPosition();
+
     /** Coordinate translation methods **/
     Vec2i toAbsolute(Vec2i);
 
-  protected:
-    //TODO: Implement mapping
+    int getLocalPose(localized_pose_t&);
+    int getGlobalPosition(Vec2d&);
 
-    LandmarkStream mLandmarkStr;
+  protected:
+    bool m_init;
+    Navigator* gNavi;
+
+    Vec2d m_gps;
+    time_ms_t m_gps_t;
+
+    localized_pose_t m_pose;
   };
 
 }
--- system/src/tuum_localizer.cpp
+++ system/src/tuum_localizer.cpp
@@ -1,4 +1,11 @@
 
+#include <cmath>
+
+#include "tuum_hal.hpp"
+
+#include "sensors/GPS.hpp"
+
+#include "tuum_navigator.hpp"
 #include "tuum_localizer.hpp"
 
 namespace tuum {
@@ -6,9 +13,14 @@ namespace tuum {
   Subsystem::TypeVar Localizer::Type;
 
   Localizer::Localizer():
-    Subsystem("Localizer")
+    Subsystem("Localizer"),
+    m_init(false),
+    m_gps_t(0)
   {
-
+    m_pose.coord.x = 0;
+    m_pose.coord.y = 0;
+    m_pose.orient = 0.0;
+    m_pose.map_id = 0;
   }
 
   Vec2i Localizer::toAbsolute(Vec2i v) {
@@ -16,12 +28,65 @@ namespace tuum {
     return v - Vec2i({200, 200});
   }
 
+  int Localizer::updateGlobalPosition()
+  {
+    if(millis() - m_gps_t < 1000) return 0; // Is limiting neccesary?
+
+    auto ptr = hal::hw.getSensors();
+    hal::GPSData dat = ptr->getGPS();
+
+    m_gps.x = dat.lat;
+    m_gps.y = dat.lng;
+    m_gps_t = dat._t;
+
+    if(millis() - m_gps_t > 5000) return 0;
+
+    return 1;
+  }
+
   void Localizer::setup() {
+    int err_flag = 0;
 
+    if(getSubsystemHandle<tuum::Navigator*>(Navigator::GetType(), gNavi) > 0) {
+      RTXLOG("Navigator present.");
+    } else err_flag = -1;
+
+    if(err_flag < 0) {
+      RTXLOG(tuum::format("Error - Missing module. (code=%i)", err_flag), LOG_ERR);
+    } else {
+      m_init = true;
+    }
   }
 
-  void Localizer::process() {
+  void Localizer::process()
+  {
+    if(updateGlobalPosition() > 0) {
+      auto ptr = gNavi->getMap();
+      Vec2d anchor = gps_round_to_km(m_gps);
 
+      if(!ptr->isAnchorFixed()) {
+        printf("anchor: (%.12f, %.12f) -> (%.12f, %.12f)\n", m_gps.x, m_gps.y, anchor.x, anchor.y);
+        ptr->updateAnchor(anchor);
+      }
+
+      m_pose.map_id = gNavi->getMap()->getId();
+      m_pose.coord = (Vec2i)gps_calc_diff_vec(anchor, m_gps);
+    }
   }
+
+  int Localizer::getLocalPose(localized_pose_t& out)
+  {
+    out = m_pose;
+    return 0;
+  }
+
+  int Localizer::getGlobalPosition(Vec2d& out) {
+    int res = updateGlobalPosition();
+    out = m_gps;
+    return res;
+  }
+
+
+
 
 }
