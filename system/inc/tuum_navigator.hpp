aa32d25ad97c4f4e357cd35e4e10d4f9d4ef9242
Meelik Kiik <kiik.meelik@gmail.com>
2017-07-17 13:42:59 +0300

    Update map handling. Add pather middleware.

--- system/inc/tuum_navigator.hpp
+++ system/inc/tuum_navigator.hpp
@@ -12,6 +12,7 @@
 
 #include "loc_landmark_stream.hpp"
 
+#include "tuum_buff.hpp"
 #include "tuum_system.hpp"
 
 using namespace loc;
@@ -19,6 +20,7 @@ using namespace loc;
 namespace tuum {
 
   class Localizer;
+  class Navigator;
 
   struct map_octree_t {
     int x = 0, y = 0, w = 1000, h = 1000;
@@ -29,9 +31,42 @@ namespace tuum {
   class EnvironmentMap
   {
   public:
-    EnvironmentMap() {}
+    EnvironmentMap():
+      m_anchor_fixed(false),
+      m_anchor({0, 0})
+    {
+
+    }
+
+    void  setAnchor(Vec2d v) { m_anchor = v; m_anchor_fixed = true; }
+    Vec2d getAnchor() { return m_anchor; }
+    bool isAnchorFixed() { return m_anchor_fixed; }
+
+    Vec2i globalToMap(const Vec2d&);
+    Vec2d mapToGlobal(const Vec2i&);
 
     virtual json toJSON() = 0;
+
+  protected:
+    bool m_anchor_fixed;
+    Vec2d m_anchor;
+  };
+
+  class EnvElem
+  {
+  public:
+    EnvElem(EnvironmentMap* ptr):
+      gEnv(ptr)
+    {
+
+    }
+
+    Vec2i getMapCoord() { return m_pos; }
+    Vec2d getGlobalCoord() { return gEnv->mapToGlobal(m_pos); }
+
+  protected:
+    EnvironmentMap *gEnv;
+    Vec2i m_pos;
   };
 
   typedef std::vector<EnvironmentMap*> EnvMapPtrSet;
@@ -48,11 +83,6 @@ namespace tuum {
 
     int getId() { return m_id; }
 
-    bool isAnchorFixed();
-    void updateAnchor(Vec2d);
-
-    Vec2d getAnchor() { return m_gps_anchor; }
-
     json toJSON();
 
   protected:
@@ -60,9 +90,62 @@ namespace tuum {
 
     map_octree_t m_root;
 
-    Vec2d m_gps_anchor;
     Vec2d m_gps_rect[4];
-    bool m_gps_anchor_fixed;
+  };
+
+
+  /** Manages collections of local navigation meshes lazily loaded when necessary **/
+  class NavmeshComposer
+  {
+  public:
+    NavmeshComposer(Navigator*);
+
+    //TODO: Dynamic area support
+    int setStaticArea(rect_t);
+
+    //int addBounds(rect_t, EnvironmentMap*);
+    int addVirtualBounds(const std::vector<Vec2i>*);
+
+    void clearBounds();
+    void clearVirtBounds();
+
+    image_t* getLocalFrame() { return &m_frame; }
+
+    void updateLocalFrame(Vec2i pos);
+
+  protected:
+    Navigator *gNav;
+    rect_t m_rect;  // Area relative to current pose position
+
+    image_t m_bL, m_vbL;
+    image_t m_frame;
+  };
+
+  class Pather
+  {
+  public:
+    typedef Vec2i wp_t;
+    typedef std::vector<wp_t> path_t;
+
+  public:
+    Pather();
+
+    int init(rect_t);
+
+    void poseTick(Vec2i, float);
+    wp_t pathTick(Vec2i, float);
+
+  protected:
+    bool m_valid_path;
+    path_t m_path;
+
+    rect_t m_bounds;
+
+    wp_t m_pos;
+    float m_ori;
+
+    wp_t m_tPos;
+    float m_tOri;
   };
 
   class Navigator : public Subsystem
@@ -93,6 +176,10 @@ namespace tuum {
 
     OctreeLandmarkMap* getMap() { return &mMap; }
 
+    NavmeshComposer* getComposer() { return &mComposer; }
+
+    Pather* getPather() { return &mPather; }
+
     int init();
 
     void setup();
@@ -102,20 +189,30 @@ namespace tuum {
     int navTo(const Vec2i&, const float&);
     // int navTo(const float&);
 
+    int isReachable(Vec2i);
+
+    int setBounds(std::vector<Vec2i>*);
+
     ctx_t getContext() { return m_ctx; }
 
     int feedLandmarks(LandmarkSet*);
 
     int getLocalMaps(EnvMapPtrSet&);
 
+    int globalToMap(const Vec2d&, Vec2i);
+    int mapToGlobal(const Vec2i&, Vec2d);
+
   protected:
+    bool m_init;
     ctx_t m_ctx;
 
     Localizer *gLoc;
 
-    bool m_init;
+    NavmeshComposer mComposer;
+    Vec2d m_local_anchor;
 
     OctreeLandmarkMap mMap;
+    Pather mPather;
   };
 
 }
